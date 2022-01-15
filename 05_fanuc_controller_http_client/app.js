02664dad3531478e73a503c17683971f03499fdf
GranrRa <97756394+GranrRa@users.noreply.github.com>
2022-01-15 19:24:49 +0200

    lisätty toinen haettava tietue

--- 05_fanuc_controller_http_client/app.js
+++ 05_fanuc_controller_http_client/app.js
@@ -12,18 +12,36 @@ app.get("/robot/joint_values", (req, res, next) => {
         (arvot_res)=>{
        
         const regexp1 = 'Joint   [1-6]: *(-?.*)';
-        let joint_values = [];
-        let matches = arvot_res.data.matchAll(regexp1);
-        let count = 0;
-        for (const match of matches){
-            count++;
-            if (count > 6) break;
-            const value = parseFloat(match[1]);
-            joint_values.push(value);
+        const regexp2 = 'VERSION : *.* ';
+        let joint_values1 = [];
+        let joint_values2 = [];
+        let joint_values3 = "__";
+        let matches1 = arvot_res.data.matchAll(regexp1);
+        let matches2 = arvot_res.data.matchAll(regexp2);
+
+    
+        let count2 = 0;
+        for (const match2 of matches2){
+            count2++;
+            //if (count2 > 1) break;
+            //const value = parseFloat(match2[1]);
+            joint_values2.push(match2);
+        }
+
+        //res.send(joint_values2);
+        //console.log(joint_values2); //Kirjoitetaan arvot myös komentoikkunaan
+
+
+        let count1 = 0;
+        for (const match1 of matches1){
+            count1++;
+            if (count1 > 6) break;
+            const value = parseFloat(match1[1]);
+            joint_values1.push(value);
         }
             
-            res.send(joint_values);
-            console.log(joint_values); //Kirjoitetaan arvot myös komentoikkunaan
+            res.send(joint_values2 + joint_values3 + joint_values1);
+            console.log(joint_values2 + joint_values3 + joint_values1); //Kirjoitetaan arvot myös komentoikkunaan
         });
     });
 
