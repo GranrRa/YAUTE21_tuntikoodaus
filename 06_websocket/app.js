14db1ac0c1167bd6b9e2e0470bc988aa73ab0811
GranrRa <97756394+GranrRa@users.noreply.github.com>
2022-01-28 16:58:43 +0200

    fdff

--- 06_websocket/app.js
+++ 06_websocket/app.js
@@ -1,6 +1,7 @@
+//tämä pyörii serverin päässä
 const express = require("express");
 const app = express();
-const server = require("http").createServer(express);
+const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 8081;
@@ -11,6 +12,9 @@ let number_of_connections = 0;
 io.on("connection", (socket)=>{
     connections[socket.id] = socket;
     number_of_connections++;
+
+    socket.emit("server-to-client", "Hello from server");
+
     console.log("client connected. number_of_connections: ", number_of_connections);
     socket.on("disconnect", (socket)=>{
         delete connections[socket.id];
@@ -19,6 +23,10 @@ io.on("connection", (socket)=>{
         //
     });
 
+    socket.on("client-to-server", (message)=>{
+        console.log(message);
 });
+
+app.use(express.static("public"));
 
 server.listen(PORT);
