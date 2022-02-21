ca71ff0f36193a8f2c2b7586fd6c62513a881256
GranrRa <97756394+GranrRa@users.noreply.github.com>
2022-02-21 16:05:14 +0200

    commitointia

--- 08_chat_application/public/client.js
+++ 08_chat_application/public/client.js
@@ -14,6 +14,7 @@ socket.on("server-to-client", (message)=>{
     //socket.emit("client-to-server", "Hello from client");
 });
 
+
 const send_message = ()=>{
     const message_input = document.getElementById("message_input");
     const name = document.getElementById("name");
